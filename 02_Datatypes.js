98e94ed2e418cad8f1dd48240fb58adefc463b5b
seaikh91-Asif <seaikh91@gmail.com>
2026-07-09 11:47:42 +0530

    datatypes in javascript

---
+++ 02_Datatypes.js
@@ -0,0 +1,21 @@
+/* 
+    number : 2 to the power 53
+    bigint
+    string : " "
+    boolean: true / false
+    null: standalone value / actuall it is a object
+    undefined: 
+    symbol: 
+
+    object
+*/
+let number = 10; 
+let name = "asif"; 
+ 
+
+let a = null; 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); //Object
+
+console.table([a, name, number]); 
