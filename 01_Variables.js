430315382f70ab72e73b94c7b182323353b878db
seaikh91-Asif <seaikh91@gmail.com>
2026-07-09 11:25:45 +0530

    variables in javascript

---
+++ 01_Variables.js
@@ -0,0 +1,9 @@
+let AccountName = "first accout"
+
+const accountAddress = "permanet address"
+
+var accountMail = "accout@google.com"
+
+let accoutDetails = "eCommers website"
+
+console.table([AccountName, accountAddress, accountMail, accoutDetails])
