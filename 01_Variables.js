90a967a88eec73640e8dc517b539a2f808cb88d0
seaikh91-Asif <seaikh91@gmail.com>
2026-07-09 11:28:14 +0530

    variable in java script

--- 01_Variables.js
+++ 01_Variables.js
@@ -1,3 +1,8 @@
+/*
+ Note: prefer not to use var 
+       always try to prefer const and let for block scope and functional scope purpose.
+*/
+
 let AccountName = "first accout"
 
 const accountAddress = "permanet address"
@@ -7,3 +12,4 @@ var accountMail = "accout@google.com"
 let accoutDetails = "eCommers website"
 
 console.table([AccountName, accountAddress, accountMail, accoutDetails])
+
