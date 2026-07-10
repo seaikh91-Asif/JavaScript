4cedf8c02f4d6b4e8a1f988e8248a7f5ebdf538a
seaikh91-Asif <seaikh91@gmail.com>
2026-07-10 10:33:43 +0530

    conversion of datatype in javascript

---
+++ 03_ConversionOperation.js
@@ -0,0 +1,23 @@
+// converting 1 to boolean:
+let isLoggedIn = 1 
+
+let booleanIsLoggedI = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedI); 
+
+
+// converting number to string:
+let someNumber = 1; 
+
+let stringNumber = String(someNumber)
+console.log( typeof stringNumber)
+
+/*
+    Note: 
+    "33": easily converted to 33
+    "33abc": converted to NaN
+    true: converted to 1; false: 0
+
+    1: true
+    "": false
+    "anything": true
+ */
