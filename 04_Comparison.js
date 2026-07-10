69f5a8578aaefb2b7d74a1464f035b3fe3f8a561
seaikh91-Asif <seaikh91@gmail.com>
2026-07-10 11:14:48 +0530

    basic comparison in javascript

---
+++ 04_Comparison.js
@@ -0,0 +1,7 @@
+console.log("hello world"); 
+
+/*
+    this topic about normal comparison in colsole area
+    clean code: we have to focus on writting clean code not 
+                writting messsy code, always focus on human readable code 
+*/
